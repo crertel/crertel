bda1d4477d04c9b2d8fde5518827ddb6ce995406
--global <--global>
2019-05-28 21:00:47 -0400

    Finish deploy pipeline.

--- bin/blog_gen.sh
+++ bin/blog_gen.sh
@@ -2,9 +2,18 @@
 
 set -eou pipefail
 
-BLOG_TITLE="Swizzled Bits"
+BLOG_TITLE="Minor Gripe"
 BLOG_DIRECTORY="$HOME/.blog"
 BLOG_BUILD_PATH="$HOME/.blog_output"
+BLOG_URL="https://www.minor.gripe"
+BLOG_DESCRIPTION="Assorted writings on life, engineering, and software."
+BLOG_POSTS_ROOT="https://www.minor.gripe/posts/"
+
+PROD_HOSTNAME="minor.gripe"
+PROD_USER="crertel"
+PROD_BLOG_PATH="/home/crertel/blog"
+PROD_TEMP_ARTIFACT="`mktemp`.tar.gz"
+
 ZAMRAZAC_PATH="$HOME/projects/zamrazac"
 
 mkdir -p "$BLOG_DIRECTORY"
@@ -19,5 +28,11 @@ popd () {
 }
 
 pushd "$ZAMRAZAC_PATH"
-BLOG_TITLE="$BLOG_TITLE" BLOG_DIRECTORY="$BLOG_DIRECTORY" OUTPUT_DIRECTORY="$BLOG_BUILD_PATH" mix zamrazac.generate
+BLOG_URL="$BLOG_URL" BLOG_DESCRIPTION="$BLOG_DESCRIPTION" BLOG_POSTS_ROOT="$BLOG_POSTS_ROOT" BLOG_TITLE="$BLOG_TITLE" BLOG_DIRECTORY="$BLOG_DIRECTORY" OUTPUT_DIRECTORY="$BLOG_BUILD_PATH" mix zamrazac.generate
 popd
+
+
+tar -czf "$PROD_TEMP_ARTIFACT" --exclude "./images" -C "$BLOG_BUILD_PATH" .
+scp "$PROD_TEMP_ARTIFACT" "$PROD_USER@$PROD_HOSTNAME:$PROD_BLOG_PATH/deploy.tar.gz"
+rm "$PROD_TEMP_ARTIFACT"
+ssh "$PROD_USER@$PROD_HOSTNAME" tar -xf "$PROD_BLOG_PATH/deploy.tar.gz" -C "$PROD_BLOG_PATH"
